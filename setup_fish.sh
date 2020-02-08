371383d2110d0203803fa068e37382a0677a86f9
CJ Pais <cjpaisdev@gmail.com>
2020-02-08 10:20:03 -0800

    add initial fish setup

---
+++ setup_fish.sh
@@ -0,0 +1,11 @@
+#!/bin/fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fisher add jethrokuan/fzf
+fisher add edc/bass
+fisher add oh-my-fish/plugin-grc
+fisher add jorgebucaran/fish-nvm
+fisher add markcial/upto
+fisher add jethrokuan/z
+
+# Theme?
+# fisher add rafaelrinaldi/pure
